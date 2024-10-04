dcd22ca1a778e09e10bea9b151ec56e52bc97176
Jayce Meyer <jnmeyer@nps.gov>
2024-10-04 10:50:13 -0600

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NPMapTrail from '@npmap/trail'
 import { 
-    MOCK_FEATURE_POINT_OF_INTEREST, 
-    MOCK_FEATURE_TRAIL_ALERTS, 
-    MOCK_FEATURE_TRAIL_DATA_WITH_ELEVATION, 
-    myElevationProfileOptions 
+  CUSTOM_ELEVATION_PROFILE_OPTIONS,
+    MOCK_FEATURE_S_RIM_VISITOR_CENTER,
+    MOCK_OAK_FLAT_TRAIL_ALERT_DATA,
+    MOCK_OAK_FLAT_TRAIL_DATA_WITH_ELEVATION, 
 } from './mock-constants'
 
 function App() {
@@ -32,14 +31,14 @@ function App() {
             <img src={viteLogo} className="logo" alt="Vite logo" />
             {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
           </div>
-          <button onClick={() => manuallyShowFeature(MOCK_FEATURE_POINT_OF_INTEREST)} style={{padding: '20px', marginRight: '10px'}}>Example of manually showing popup of feature</button>
+          <button onClick={() => manuallyShowFeature(MOCK_FEATURE_S_RIM_VISITOR_CENTER)} style={{padding: '20px', marginRight: '10px'}}>Example of manually showing popup of feature</button>
         </div>
         
         <NPMapTrail 
-            // trailDataUrl={'https://svcdev.nps.gov/data/api/v1/sites?apikey=KXuXrDdge2Csv0xbC01JhhNNaDGcmICX&format=geojson&type=trail&select=relatedsites,icon,geometry,id,description,trailinfo,amenities,images&code=ROMO'}
-            trailDataUrl={MOCK_FEATURE_TRAIL_DATA_WITH_ELEVATION}
+            // trailDataUrl={'https://svcdev.nps.gov/data/api/v1/sites?apikey=KXuXrDdge2Csv0xbC01JhhNNaDGcmICX&format=geojson&type=trail&select=relatedsites,icon,geometry,id,description,trailinfo,amenities,images&id=d0e3dc08-9a33-ea37-8c00-4d4cf9fa7d4c'}
+            trailDataUrl={MOCK_OAK_FLAT_TRAIL_DATA_WITH_ELEVATION}
             // trailAlertsUrl={'https://svcdev.nps.gov/data/api/v1/trdx/feeds?apikey=KXuXrDdge2Csv0xbC01JhhNNaDGcmICX&format=geoJson'} // Optional - URL for trail alerts, defaults to all alerts from PROD
-            trailAlertsUrl={MOCK_FEATURE_TRAIL_ALERTS}
+            trailAlertsUrl={MOCK_OAK_FLAT_TRAIL_ALERT_DATA}
             featurePopupToShow={featureToShow} // Optional - Use to show a map popup for a feature (if contained in the data from trailDataUrl)
             elementClicked={handleElementClicked} // Optional. Use to capture a clicked map feature
             mapCssHeight={'100%'} // Optional, defaults to 98vh - if the parent element of the map uses "%" for height, then this cannot be a "%" value
@@ -47,7 +46,7 @@ function App() {
             enableTrailAlerts={true}  // Optional, defaults to true
             enableTrailAlertsToggle={true}  // Optional, defaults to true (but relies on enableTrailAlerts being true)
             enableElevationProfile={true}  // Optional, defaults to true
-            elevationProfileOptions={myElevationProfileOptions} // Optional, has default styling
+            // elevationProfileOptions={CUSTOM_ELEVATION_PROFILE_OPTIONS} // Optional, has default styling
             // useTESTElevation={true}
         />
       </div>
